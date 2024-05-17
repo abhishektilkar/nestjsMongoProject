7515794f218ccffd1502018f9190245cd2ec5b08
abhishektilkar <tilkarabhishek2000@gmail.com>
2024-05-17 23:47:22 +0530

    Add IsOptional decorator to optional fields in dtos all that

--- src/users/dto/CreateUser.dto.ts
+++ src/users/dto/CreateUser.dto.ts
@@ -1,12 +1,14 @@
-import { IsNotEmpty, IsString } from "class-validator";
+import { IsNotEmpty, IsOptional, IsString } from "class-validator";
 
 export class CreateUserDto {
     @IsNotEmpty()
     @IsString()
     username: String;
 
+    @IsOptional()
     @IsString()
     displayName?: String;
     
+    @IsOptional()
     avatar?: String;
 }
--- src/users/dto/UpdateUser.dto.ts
+++ src/users/dto/UpdateUser.dto.ts
@@ -1,12 +1,15 @@
-import { IsNotEmpty, IsString } from "class-validator";
+import { IsNotEmpty, IsOptional, IsString } from "class-validator";
 
 export class UpdateUserDto {
 
+    @IsOptional()
     @IsString()
     username?: String;
 
+    @IsOptional()
     @IsString()
     displayName?: String;
     
+    @IsOptional()
     avatar?: String;
 }
